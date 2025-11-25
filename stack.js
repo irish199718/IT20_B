298653ff5d3d5bff191aaad8800f885afe1bda65
irish199718 <gudesirish2@gmail.com>
2025-11-25 08:50:31 +0800

    Added stack element

--- stack.js
+++ stack.js
@@ -33,6 +33,16 @@ class Stack {
         if(this.is_empty()) return null;
         return this.data[this.topIndex];
     }
+
+    pop() {
+        //Correcting Statement
+        if(this.is_empty()) return null;
+
+        let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+    }
         
 }
 
@@ -45,6 +55,7 @@ console.log(stack1.is_empty());
 //stack1.push(3);
 //stack1.push(5);
 //stack1.push(8);
-console.log(stack1.is_empty());
-console.log(stack1.size());
+console.log("The current size is "+ stack1.size());
 console.log("The top element is: " + stack1.peek());
+console.log("top element is: " + stack1.pop() +" is popped, the new top is: " + stack1.peek());
+console.log("The new stack size is: " + stack1.size());
