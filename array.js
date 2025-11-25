b8d95c98f0b35a16dfd8a4355462989a00886c0b
irish199718 <gudesirish2@gmail.com>
2025-11-25 08:11:41 +0800

    Added delete method for DSA/array.js

--- array.js
+++ array.js
@@ -20,6 +20,40 @@ traverse() {
     console.log(output);
 }
 
+insert(index, element) {
+    //Correcting Statement || Must be false
+    if(index < 0 || index >= this.length) {
+        console.log("Invalid Index");
+        return;
+    }
+
+    //Shifting Condition
+    for (let i = this.length; i > index; i--) {
+        this.data[i] = this.data[i - 1];
+    }
+
+    //After Condition
+    this.data[index] = element;
+    this.length++;
+}
+ 
+delete(index) {
+
+     //Correcting Statement || Must be false
+    if(index < 0 || index >= this.length) {
+        console.log("Invalid Index");
+        return;
+    }
+
+    //Shifting Condition
+    for (let i = index; i < this.length - 1; i++) {
+        this.data[i] = this.data[i + 1];
+    }
+
+    //After Condition
+    delete this.data[this.length - 1];
+    this.length--;
+}
 }
 
 //
@@ -28,3 +62,6 @@ traverse() {
 arr1 = new Array([4,2,3,]);
 
 arr1.traverse(); 
+arr1.insert(0,9);
+arr1.traverse();
+arr1.delete(5);
