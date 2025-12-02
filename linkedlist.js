5126349806e9fc2d32c31831d677abf6fbb8b102
irish199718 <gudesirish2@gmail.com>
2025-12-02 08:36:38 +0800

    added queue

---
+++ linkedlist.js
@@ -0,0 +1,42 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+
+    }
+}
+
+
+class Linkedlist{
+
+    constructor(){
+        this.head = null;
+    }
+    insert_at_beginning(){
+        const node = new Node(element);
+        node.next = this.head;
+        this.head = node;
+    }
+
+    traverse(){
+        let current = this.head;
+        let output = "";
+
+        while(current){
+            output += current.value + " ";
+            current = current.next;
+        }
+
+        return output;
+    }
+}
+
+
+
+
+
+
+
+
+
+let list1
