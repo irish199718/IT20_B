1e97b351da5b310aed4316b17e03278897628899
irish199718 <gudesirish2@gmail.com>
2025-12-02 08:51:43 +0800

    added linkedlist

---
+++ linkedlist.js
@@ -0,0 +1,19 @@
+class Node{
+constructor(value){
+this.value = value;
+this.next = null;
+
+}
+}
+
+
+class Linkedlist{
+
+constructor(){
+this.head = null;
+}
+insert_at_beginning(){
+const node = new Node(element);
+node.next = this.head;
+this.head = node;
+}
